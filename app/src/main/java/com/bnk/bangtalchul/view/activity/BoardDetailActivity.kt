62e9cb5e4fbf27b85d8542d2b260359acb352f47
kongsubin <qls0919@naver.com>
2022-08-25 13:03:53 +0900

    :recycle: 데이터 선언 방식 변경

--- app/src/main/java/com/bnk/bangtalchul/view/activity/BoardDetailActivity.kt
+++ app/src/main/java/com/bnk/bangtalchul/view/activity/BoardDetailActivity.kt
@@ -11,10 +11,6 @@ import com.bnk.bangtalchul.viewmodel.BoardViewModel
 
 class BoardDetailActivity : AppCompatActivity() {
     lateinit var board : BoardEntity
-    private lateinit var linearLayout : LinearLayout
-    private lateinit var title : TextView
-    private lateinit var content : TextView
-
     private lateinit var boardViewModel: BoardViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +19,9 @@ class BoardDetailActivity : AppCompatActivity() {
 
         board = intent.getSerializableExtra("board") as BoardEntity
 
-        linearLayout = findViewById(R.id.chatpage_bottomsheet)
-        title = findViewById(R.id.chatpage_title_textview)
-        content = findViewById(R.id.chatpage_expandable_layout);
+        val linearLayout = findViewById<LinearLayout>(R.id.chatpage_bottomsheet)
+        val title = findViewById<TextView>(R.id.chatpage_title_textview)
+        val content = findViewById<TextView>(R.id.chatpage_expandable_layout);
 
         linearLayout.bringToFront()
         title.text = board.title
