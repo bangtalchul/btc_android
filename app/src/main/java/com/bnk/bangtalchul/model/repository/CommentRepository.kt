c1b477c4c3d4911b0a40006e095989e789025b5c
kongsubin <qls0919@naver.com>
2022-08-28 19:13:17 +0900

    :lipstick: 커뮤니티 상세페이지 댓글 ui 수정

--- app/src/main/java/com/bnk/bangtalchul/model/repository/CommentRepository.kt
+++ app/src/main/java/com/bnk/bangtalchul/model/repository/CommentRepository.kt
@@ -7,6 +7,11 @@ class CommentRepository {
     var comment = CommentEntity(0, 0, "", 0,"", "")
 
     private val commentList :ArrayList<CommentEntity> = arrayListOf(
+        CommentEntity(1, 2, "코틀린", 0, "박상혁", "20220815"),
+        CommentEntity(2, 2, "재밌다^^", 0, "박상혁", "20220715"),
+        CommentEntity(3, 2, "힝", 1, "손수현", "20220615"),
+        CommentEntity(4, 2, "구", 1, "손수현", "20220515"),
+        CommentEntity(4, 2, "구", 1, "손수현", "20220515"),
         CommentEntity(1, 2, "힝", 0, "박상혁", "20220815"),
         CommentEntity(2, 2, "구", 0, "박상혁", "20220715"),
         CommentEntity(3, 2, "힝", 1, "손수현", "20220615"),
