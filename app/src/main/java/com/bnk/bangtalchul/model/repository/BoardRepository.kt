7b2c5a75c9f9f6153f8b20aa2421aff5919f621e
kongsubin <qls0919@naver.com>
2022-09-04 21:19:14 +0900

    :sparkles: 게시글 삭제 및 수정 버튼 기능 추가

--- app/src/main/java/com/bnk/bangtalchul/model/repository/BoardRepository.kt
+++ app/src/main/java/com/bnk/bangtalchul/model/repository/BoardRepository.kt
@@ -7,15 +7,21 @@ import com.bnk.bangtalchul.model.entity.BoardEntity
 class BoardRepository() {
     var board = BoardEntity(0, "", "", 0, "", 0)
 
-    private val boardList :ArrayList<BoardEntity> = arrayListOf(
+    private var boardList :ArrayList<BoardEntity> = arrayListOf(
         BoardEntity(1, "팟 구해여", "포항사는 방탈출 러버 찾아요", 5, "20220815", 0),
         BoardEntity(2, "부산 서면 방탈출~~", "이번주 토욜 갈 사람 구함~", 4, "20220715", 0),
         BoardEntity(3, "근데 방탈출 할 때..", "금데 방탈출 할 때...", 3, "20220615", 0),
         BoardEntity(4, "음주 방탈출", "술마시고 방탈출 못하나?", 5,"20220515", 0),
-        BoardEntity(5, "팟", "서울 팟 구함", 6,"20220415", 1)
+        BoardEntity(5, "팟", "서울 팟 구함", 6,"20220415", 1),
+        BoardEntity(6, "팟 구해여", "포항사는 방탈출 러버 찾아요", 5, "20220815", 0),
+        BoardEntity(7, "부산 서면 방탈출~~", "이번주 토욜 갈 사람 구함~", 4, "20220715", 0),
+        BoardEntity(8, "근데 방탈출 할 때..", "금데 방탈출 할 때...", 3, "20220615", 0),
+        BoardEntity(9, "음주 방탈출", "술마시고 방탈출 못하나?", 5,"20220515", 0),
+        BoardEntity(10, "팟", "서울 팟 구함", 6,"20220415", 1)
+
     )
 
-    private val myBoardList :ArrayList<BoardEntity> = arrayListOf(
+    private var myBoardList :ArrayList<BoardEntity> = arrayListOf(
         BoardEntity(1, "팟 구해여", "포항사는 방탈출 러버 찾아요", 5, "20220815", 0),
         BoardEntity(2, "부산 서면 방탈출~~", "이번주 토욜 갈 사람 구함~", 4, "20220715", 0),
         BoardEntity(3, "근데 방탈출 할 때..", "금데 방탈출 할 때...", 3, "20220615", 0),
--- app/src/main/java/com/bnk/bangtalchul/view/activity/BoardDetailActivity.kt
+++ app/src/main/java/com/bnk/bangtalchul/view/activity/BoardDetailActivity.kt
@@ -2,14 +2,17 @@ package com.bnk.bangtalchul.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bnk.bangtalchul.R
 import com.bnk.bangtalchul.model.entity.BoardEntity
 import com.bnk.bangtalchul.model.repository.CommentRepository
+import com.bnk.bangtalchul.model.repository.UserInfoRepository
 import com.bnk.bangtalchul.view.adapter.BoardAdapter
 import com.bnk.bangtalchul.view.adapter.CommentAdapter
 import com.bnk.bangtalchul.viewmodel.BoardViewModel
@@ -18,6 +21,7 @@ class BoardDetailActivity : AppCompatActivity() {
     lateinit var board : BoardEntity
     private lateinit var boardViewModel: BoardViewModel
     private val commentRepository = CommentRepository()
+    private val userInfoRepository = UserInfoRepository()
 
     private var linearLayoutManager: RecyclerView.LayoutManager? = null
     private lateinit var recyclerAdapter: CommentAdapter
@@ -28,16 +32,41 @@ class BoardDetailActivity : AppCompatActivity() {
 
         // board info setting on view
         board = intent.getSerializableExtra("board") as BoardEntity
+        boardViewModel = BoardViewModel()
 
         val linearLayout = findViewById<LinearLayout>(R.id.chatpage_bottomsheet)
         val title = findViewById<TextView>(R.id.chatpage_title_textview)
         val content = findViewById<TextView>(R.id.chatpage_expandable_layout)
+        val editButton = findViewById<ImageButton>(R.id.edit_button)
+        val deleteButton = findViewById<ImageButton>(R.id.delete_button)
 
         linearLayout.bringToFront()
         title.text = board.title
         content.text = board.content
 
-        boardViewModel = BoardViewModel()
+        if(board.writerId != userInfoRepository.user.userId){
+            editButton.visibility = View.GONE
+            deleteButton.visibility = View.GONE
+        }
+        editButton.setOnClickListener {
+
+        }
+        deleteButton.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("해당 게시글을 삭제하겠습니까?")
+                .setNegativeButton("NO") { _, _ -> }
+                .setPositiveButton("YES") { _, _ ->
+                    boardViewModel.delete(board.boardId)
+                    finish()
+                }
+            builder.show()
+        }
+
+
+
+
+
+
 
 
         //back button
