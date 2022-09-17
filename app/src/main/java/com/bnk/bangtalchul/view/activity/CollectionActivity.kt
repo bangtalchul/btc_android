bf9581933e0cec45d6e40be1a791d1e199b0e57c
kongsubin <qls0919@naver.com>
2022-09-17 20:51:56 +0900

    :sparkles: Collection 페이지 추가

---
+++ app/src/main/java/com/bnk/bangtalchul/view/activity/CollectionActivity.kt
@@ -0,0 +1,12 @@
+package com.bnk.bangtalchul.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bnk.bangtalchul.R
+
+class CollectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
