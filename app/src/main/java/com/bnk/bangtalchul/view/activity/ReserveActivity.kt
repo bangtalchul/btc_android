a162af8c6d2ac08012f9c300c4fac6170f82b0ea
kongsubin <qls0919@naver.com>
2022-09-27 19:24:04 +0900

    :lipstick: 캘린더 뷰

--- app/src/main/java/com/bnk/bangtalchul/view/activity/ReserveActivity.kt
+++ app/src/main/java/com/bnk/bangtalchul/view/activity/ReserveActivity.kt
@@ -2,20 +2,40 @@ package com.bnk.bangtalchul.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.bnk.bangtalchul.R
+import com.bnk.bangtalchul.view.adapter.AdapterMonth
 import com.bnk.bangtalchul.viewmodel.ThemeViewModel
 
 class ReserveActivity : AppCompatActivity() {
 
     private lateinit var themeViewModel: ThemeViewModel
 
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_reserve)
+//
+//        themeViewModel = ThemeViewModel()
+//
+//        val themeId = intent.getStringExtra("theme_id")?.toInt()?: 0
+//        var themeInfo = themeViewModel.getReserveList(themeId)
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reserve)
 
-        themeViewModel = ThemeViewModel()
+        val monthListManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val monthListAdapter = AdapterMonth()
 
-        val themeId = intent.getStringExtra("theme_id")?.toInt()?: 0
-        var themeInfo = themeViewModel.getReserveList(themeId)
+        val calendar_custom = findViewById<RecyclerView>(R.id.calendar_custom)
+        calendar_custom.apply {
+            layoutManager = monthListManager
+            adapter = monthListAdapter
+            scrollToPosition(Int.MAX_VALUE/2)
+        }
+        val snap = PagerSnapHelper()
+        snap.attachToRecyclerView(calendar_custom)
     }
 }
---
+++ app/src/main/java/com/bnk/bangtalchul/view/adapter/AdapterDay.kt
@@ -0,0 +1,47 @@
+package com.bnk.bangtalchul.view.adapter
+
+
+import android.graphics.Color
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bnk.bangtalchul.R
+import java.util.*
+
+class AdapterDay(val tempMonth:Int, val dayList: MutableList<Date>): RecyclerView.Adapter<AdapterDay.DayView>() {
+    val ROW = 6
+
+    inner class DayView(val layout: View): RecyclerView.ViewHolder(layout)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DayView {
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_day, parent, false)
+        return DayView(view)
+    }
+
+    override fun onBindViewHolder(holder: DayView, position: Int) {
+        holder.layout.findViewById<LinearLayout>(R.id.item_day_layout).setOnClickListener {
+            Toast.makeText(holder.layout.context, "${dayList[position]}", Toast.LENGTH_SHORT).show()
+            holder.layout.findViewById<LinearLayout>(R.id.item_day_layout).setBackgroundColor(Color.parseColor("#afe3ff"))
+        }
+        holder.layout.findViewById<TextView>(R.id.item_day_text).text = dayList[position].date.toString()
+
+        holder.layout.findViewById<TextView>(R.id.item_day_text).setTextColor(when(position % 7) {
+            0 -> Color.RED
+            6 -> Color.BLUE
+            else -> Color.BLACK
+        })
+
+        if(tempMonth != dayList[position].month - 1) {
+            holder.layout.findViewById<TextView>(R.id.item_day_text).alpha = 0.4f
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ROW * 7
+    }
+}
